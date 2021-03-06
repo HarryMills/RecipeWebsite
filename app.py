a7debc115caee6315d445375db67953030aa54ce
emmahemus <emma.hemus@gmail.com>
2021-03-06 16:53:40 +0000

    created function to get and post to /recipes

--- app.py
+++ app.py
@@ -8,5 +8,13 @@ def index():
     return 'Harry and Emma\'s Recipe Website'
 
 
+@app.route('/recipes/<recipe_id>', methods=["GET", "POST"])
+def recipes(recipe_id):
+    if request.method == "GET":
+        """return the recipe information for <recipe_id>"""
+
+    if request.method == "POST":
+        """modify/update the information for <recipe_id>"""
+
 if __name__ == "__main__":
     app.run()
