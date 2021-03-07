87eaf4c23f4402b14c0ce82297569c382e69154f
Harry Mills <harry.e.mills@gmail.com>
2021-03-07 15:56:30 +0000

    draft retrieve and edit functions for backend

--- backend/database.py
+++ backend/database.py
@@ -9,14 +9,16 @@ class Database:
         val = self.manager.collection.insert_one(data)
         return val.inserted_id
 
-    def retrieve_recipe(self, data):
-        print("retrieve!")
-        # TODO: Implement
+    def retrieve_recipe(self, query):
+        # Example query could be a dictionary like below:
+        # myquery = {"id": "0001"}
+        results = self.manager.collection.find(query)
+        return results
 
-    def edit_recipe(self, data):
-        print("edit!")
-        # TODO: Implement
+    def edit_recipe(self, query, data):
+        result = self.manager.collection.update_one(query, data)
+        return result.acknowledged
 
-    def delete_recipe(self, data):
-        val = self.manager.collection.delete_one(data)
+    def delete_recipe(self, query):
+        val = self.manager.collection.delete_one(query)
         return val.deleted_count
