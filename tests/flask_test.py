111f35922061158cfc4e3467f352a411ad314de7
emmahemus <emma.hemus@gmail.com>
2021-03-07 17:47:43 +0000

    playing around with start/stop of app

--- tests/flask_test.py
+++ tests/flask_test.py
@@ -1,19 +1,42 @@
 import unittest
-from backendAPI import app, start, shutdown
+from backendAPI import app
+from multiprocessing import Process
+import time
 
 
-class AppTests(unittest.TestCase):
-    def test_start(self):
-        result = start()
-        self.assertEqual(result, "starting up the app")
-        shutdown()
+# class AppTests(unittest.TestCase):
+#     def start_app(self):
+#         server = Process(target=app.run)
+#         server.start()
+#         return server
+#
+#     def shutdown_app(self, server):
+#         server.terminate()
+#         server.join()
+#
+# #     def test_start(self):
+# #         server = self.start_app()
+# #
+# #     def test_shutdown(self):
+# #         start()
+# #         result = shutdown()
+# #         self.assertEqual(result, "Server is shutting down")
+# #
+# #
+# # if __name__ == "__main__":
+# #     unittest.main()
+#
+def start():
+    server = Process(target=app.run)
+    server.start()
+    return server
 
-    def test_shutdown(self):
-        start()
-        result = shutdown()
-        self.assertEqual(result, "Server is shutting down")
 
+def shutdown(server):
+    server.terminate()
+    server.join()
 
-if __name__ == "__main__":
-    unittest.main()
+server = start()
+time.sleep(10)
+shutdown(server)
 
