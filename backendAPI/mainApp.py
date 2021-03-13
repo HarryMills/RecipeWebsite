ae7cac6dd0d14e825a978d2b57de2b25adaa7b6b
emmahemus <emma.hemus@gmail.com>
2021-03-13 14:50:16 +0000

    collection_name won't change so removing as function variable

--- backendAPI/mainApp.py
+++ backendAPI/mainApp.py
@@ -4,31 +4,32 @@ import backend
 app = Flask(__name__)
 
 database_name = 'RecipeSite'
-# collection_name = 'recipes'
+collection_name = 'recipes'
 
 @app.route('/')
 def index():
     return 'Harry and Emma\'s Recipe Project'
 
 
-@app.route('/<collection_name>/<recipe_id>', methods=["GET"])
-def view_recipe(collection_name, recipe_id):
+@app.route('/recipes/<recipe_id>', methods=["GET"])
+def view_recipe(recipe_id):
     """return the recipe information for <recipe_id>"""
     db = backend.Database(database_name, collection_name)
     query = {"id": recipe_id}
     return db.retrieve_recipe(query)
 
 
-@app.route('/<collection_name>/create_new', methods=["POST"])
-def new_recipe(collection_name, recipe):
-    """adding recipe to collection in database"""
+@app.route('/recipes/create_new', methods=["POST"])
+def new_recipe(recipe):
+    """adding recipe to the database"""
     db = backend.Database(database_name, collection_name)
     recipe_id = db.create_recipe(recipe)
     view_recipe(recipe_id)
 
-@app.route('/<collection_name>/', methods=["GET"])
-def view_all(collection_name):
-    """viewing all recipes in a collection"""
+
+@app.route('/recipes/', methods=["GET"])
+def view_all():
+    """viewing all recipes on the site"""
     db = backend.Database(database_name, collection_name)
     return db.retrieve_all()
 
