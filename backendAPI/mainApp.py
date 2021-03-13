017a0df1fb6ef13fe9f1f9c743bbc4643ac42a43
emmahemus <emma.hemus@gmail.com>
2021-03-13 14:44:21 +0000

    added view, create and view all methods for backendapi

--- backendAPI/mainApp.py
+++ backendAPI/mainApp.py
@@ -1,21 +1,36 @@
 from flask import Flask, request
+import backend
 
 app = Flask(__name__)
 
+database_name = 'RecipeSite'
+# collection_name = 'recipes'
 
 @app.route('/')
 def index():
     return 'Harry and Emma\'s Recipe Project'
 
 
-@app.route('/recipes/<recipe_id>', methods=["GET", "POST"])
-def recipes(recipe_id):
-    if request.method == "GET":
-        """return the recipe information for <recipe_id>"""
+@app.route('/<collection_name>/<recipe_id>', methods=["GET"])
+def view_recipe(collection_name, recipe_id):
+    """return the recipe information for <recipe_id>"""
+    db = backend.Database(database_name, collection_name)
+    query = {"id": recipe_id}
+    return db.retrieve_recipe(query)
 
-    if request.method == "POST":
-        """modify/update the information for <recipe_id>"""
 
+@app.route('/<collection_name>/create_new', methods=["POST"])
+def new_recipe(collection_name, recipe):
+    """adding recipe to collection in database"""
+    db = backend.Database(database_name, collection_name)
+    recipe_id = db.create_recipe(recipe)
+    view_recipe(recipe_id)
+
+@app.route('/<collection_name>/', methods=["GET"])
+def view_all(collection_name):
+    """viewing all recipes in a collection"""
+    db = backend.Database(database_name, collection_name)
+    return db.retrieve_all()
 
 
 
