bf5a7145b007816a3ebbfb1a8012d9d41e10e384
emmahemus <emma.hemus@gmail.com>
2021-03-13 15:00:03 +0000

    added methods to remove and edit recipes

--- backendAPI/mainApp.py
+++ backendAPI/mainApp.py
@@ -21,7 +21,7 @@ def view_recipe(recipe_id):
 
 @app.route('/recipes/create_new', methods=["POST"])
 def new_recipe(recipe):
-    """adding recipe to the database"""
+    """adding recipe to the database. redirects to that recipe"""
     db = backend.Database(database_name, collection_name)
     recipe_id = db.create_recipe(recipe)
     view_recipe(recipe_id)
@@ -33,6 +33,25 @@ def view_all():
     db = backend.Database(database_name, collection_name)
     return db.retrieve_all()
 
+
+@app.route('/recipes/<recipe_id>', methods=["POST"])
+def remove_recipe(recipe_id):
+    """deleting a specific recipe. redirects to page with all recipes"""
+    db = backend.Database(database_name, collection_name)
+    query = {"id": recipe_id}
+    val = db.delete_recipe(query)
+    # do we need to do anything with val?? --> maybe for testing?
+    view_all()
+
+
+@app.route('/recipes/<recipe_id>', methods=["POST"])
+def edit_recipe(recipe_id, data):
+    """updating details for a specific recipe id, then gets edited recipe"""
+    db = backend.Database(database_name, collection_name)
+    query = {"id": recipe_id}
+    result = db.edit_recipe(query, data)
+    # do we need to do anything with result?? --> maybe for testing?
+    view_recipe(recipe_id)
 
 
 
